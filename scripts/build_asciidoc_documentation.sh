72912e07916b489387f2e2694c979563a49ea40d
“wieger” <j.w.wesselink@tue.nl>
2024-09-12 16:17:26 +0200

    Add syntax highlighting to the asciidoc documentation

--- scripts/build_asciidoc_documentation.sh
+++ scripts/build_asciidoc_documentation.sh
@@ -1,3 +1,3 @@
 OUTPUT_DIRECTORY=../output_directory
 mkdir -p $OUTPUT_DIRECTORY
-asciidoctor -r asciidoctor-bibtex -D $OUTPUT_DIRECTORY ../doc/*.adoc
+asciidoctor -r asciidoctor-bibtex -a source-highlighter=rouge -D $OUTPUT_DIRECTORY ../doc/*.adoc
