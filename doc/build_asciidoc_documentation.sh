b30e5835d5e1cc21f3b55072dc68920a32212fb5
“Wieger <j.w.wesselink@tue.nl>
2025-05-15 13:48:59 +0200

    Restructured the documentation
    - this should make it easier to reuse code

--- doc/build_asciidoc_documentation.sh
+++ doc/build_asciidoc_documentation.sh
@@ -2,4 +2,5 @@
 
 OUTPUT_DIRECTORY=./output_directory
 mkdir -p $OUTPUT_DIRECTORY
-asciidoctor -r asciidoctor-bibtex -a source-highlighter=rouge -D $OUTPUT_DIRECTORY nerva*.adoc
+asciidoctor -r asciidoctor-bibtex -a source-highlighter=rouge -D $OUTPUT_DIRECTORY python/nerva-python.adoc
+asciidoctor -r asciidoctor-bibtex -a source-highlighter=rouge -D $OUTPUT_DIRECTORY cpp/nerva-cpp.adoc
