5c6e851b9509407e2f0998d4db8a5a4e03eb7738
“Wieger <j.w.wesselink@tue.nl>
2025-08-21 14:40:26 +0200

    Fix the build_asciidoc_documentation.sh script
    - make sure that the nerva.bib file can be found if the script is run
      from the root

--- doc/build_asciidoc_documentation.sh
+++ doc/build_asciidoc_documentation.sh
@@ -12,12 +12,15 @@ mkdir -p "${OUTPUT_DIRECTORY}"
 # Pre-clean only our outputs (do not touch other artifacts like PDFs)
 rm -f "${OUTPUT_DIRECTORY}/nerva-python.html" "${OUTPUT_DIRECTORY}/nerva-rowwise.html"
 
-# Common Asciidoctor options
-BIBTEX_FILE="${SCRIPT_DIR}/latex/nerva.bib"
+# Common Asciidoctor options (no bibtex-file here)
 COMMON_OPTS=(-r asciidoctor-bibtex -a "source-highlighter=rouge" -D "${OUTPUT_DIRECTORY}")
 
 # Build Python manual
-asciidoctor "${COMMON_OPTS[@]}" -a "bibtex-file=${BIBTEX_FILE}" "${SCRIPT_DIR}/asciidoc/python/nerva-python.adoc"
+pushd "${SCRIPT_DIR}/asciidoc/python" >/dev/null
+asciidoctor "${COMMON_OPTS[@]}" -a "bibtex-file=../../latex/nerva.bib" nerva-python.adoc
+popd >/dev/null
 
 # Build C++ manual
-asciidoctor "${COMMON_OPTS[@]}" -a "bibtex-file=${BIBTEX_FILE}" "${SCRIPT_DIR}/asciidoc/cpp/nerva-rowwise.adoc"
+pushd "${SCRIPT_DIR}/asciidoc/cpp" >/dev/null
+asciidoctor "${COMMON_OPTS[@]}" -a "bibtex-file=../../latex/nerva.bib" nerva-rowwise.adoc
+popd >/dev/null
