0eb1e5ea0eb79956533188729829f7c6eece5096
“Wieger <j.w.wesselink@tue.nl>
2025-08-21 11:59:59 +0200

    Move asciidoc documentation to doc/asciidoc

--- doc/build_asciidoc_documentation.sh
+++ doc/build_asciidoc_documentation.sh
@@ -2,5 +2,5 @@
 
 OUTPUT_DIRECTORY=./output_directory
 mkdir -p $OUTPUT_DIRECTORY
-asciidoctor -r asciidoctor-bibtex -a source-highlighter=rouge -D $OUTPUT_DIRECTORY python/nerva-python.adoc
-asciidoctor -r asciidoctor-bibtex -a source-highlighter=rouge -D $OUTPUT_DIRECTORY cpp/nerva-rowwise.adoc
+asciidoctor -r asciidoctor-bibtex -a source-highlighter=rouge -D $OUTPUT_DIRECTORY asciidoc/python/nerva-python.adoc
+asciidoctor -r asciidoctor-bibtex -a source-highlighter=rouge -D $OUTPUT_DIRECTORY asciidoc/cpp/nerva-rowwise.adoc
